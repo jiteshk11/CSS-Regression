0a2a97d15899bfc4dee0247ffe09ab2e2b1057c9
Jitesh Kumar <jkumar2@MTVL130e97583.local>
2016-05-17 10:12:00 -0700

    first commit

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-
-var requireDir = require('require-dir');
-requireDir('./node_modules/backstopjs/gulp/tasks');
-
-gulp.task('default', ['scripts', 'sass'], function () {
-    gulp.watch('node_modules/backstopjs/gulp/tasks/*.js', ['scripts']);
-    gulp.watch('node_modules/backstopjs/gulp/tasks/*.{sass,scss}', ['sass']);
-});
